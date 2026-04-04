548db8410eeaa2b4f59ba415a88fdb415c7885e4
Sujalstha62 <shresthasujal842@gmail.com>
2026-04-04 12:27:45 +0545

    Complete alphabet beta version

--- alphabetdoubleloop.cpp
+++ alphabetdoubleloop.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
+#include<conio.h>
 #include<string>
-#define size 3
 using namespace std;
 void display(char ch,int n,char chara){
 	int i,j,t;
@@ -14,6 +14,22 @@ void display(char ch,int n,char chara){
 				i==n-n/3-1 //middle line
 				)cout<<chara;
 				else cout<<' ';
+			}
+			cout<<endl;
+		}
+	}
+	else if(ch=='B'|| ch=='b'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				j==0||//left line
+				(j<n-n/4&&(i==0||i==n-1||i==n/2))||//border top middle down line
+				(i<n/2&&i+j==n+n/4-1)||//middle up diagonal
+				(j-i==n/4&&i>n/2)||//middle down diagonal
+				(j!=n-1&&j-i==n-n/4-1)||//top right corner
+				2*n-i-j-2==n/4//bottom right corner
+				)cout<<chara;
+				else cout<<" ";
 			}
 			cout<<endl;
 		}
@@ -201,6 +217,20 @@ void display(char ch,int n,char chara){
 			cout<<endl;
 		}
 	}
+	else if(ch=='P'|| ch=='p'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				j==0||//left line
+				(j<n-n/4&&(i==0||i==n/2))||//border top middle line
+				(i<n/2&&i+j==n+n/4-1)||//middle up diagonal
+				(j!=n-1&&j-i==n-n/4-1)//top right corner
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
 	else if(ch=='Q' || ch=='q'){
 		for(i=0;i<n;i++){
 			for(j=0;j<n;j++){
@@ -218,6 +248,21 @@ void display(char ch,int n,char chara){
 			cout<<endl;
 		}
 	}
+	else if(ch=='R'|| ch=='r'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				j==0||//left line
+				(j<n-n/4&&(i==0||i==n/2))||//border top middle line
+				(i<n/2&&i+j==n+n/4-1)||//middle up diagonal
+				(j!=n-1&&j-i==n-n/4-1)||//top right corner
+				(i>n/2-1&&i==j)//border diagonal
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
 	else if(ch=='S'|| ch=='s'){
 		for(i=0;i<n;i++){
 			for(j=0;j<n;j++){
@@ -228,7 +273,7 @@ void display(char ch,int n,char chara){
 				i+j==n/4||//top left corner
 				(j!=n-1&&j-i==n-n/4-1)||//top right corner
 				(j!=0&&i-j==n-n/4-1)||//bottm left corner
-				2*n-i-j-2==n/4//borrom right corner
+				2*n-i-j-2==n/4//bottom right corner
 				)cout<<chara;
 				else cout<<" ";
 			}
@@ -247,49 +292,133 @@ void display(char ch,int n,char chara){
 			cout<<endl;
 		}
 	}
+	else if(ch=='U' || ch=='u'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				i-j==n-n/4-1||//bottom left corner
+				2*n-i-j-2==n/4||//bottom righ corner
+				(i<n-n/4)&&(j==0||j==n-1)||//border left right line
+				(j>n/4&&j<n-n/4)&&i==n-1//border bottom line
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	else if(ch=='V'||ch=='v'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				i-j==i/2||
+				i-n+j+1==i/2
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	else if(ch=='W'||ch=='w'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				j==0||//left line
+				j==n-1||//right line
+				(i>n/2-1&&(i==j||n-i-1==j))//border diagonals
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	else if(ch=='X'||ch=='x'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				i==j||//right diagonal
+				n-i-1==j//left diagonal
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	else if(ch=='Y'||ch=='y'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				(i>n/2&&j==n/2)||//border middle line
+				(i<n/2+1&&(i==j||n-i-1==j))//border diagonals
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	else if(ch=='Z'||ch=='z'){
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if(
+				i==0||//top line
+				i==n-1||//bottom line
+				i==n-j-1//diagonal
+				)cout<<chara;
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	else if(ch==' '){
+		for(i=0;i<n/2;i++){
+			for(j=0;j<n;j++){
+				cout<<" ";
+			}
+			cout<<endl;
+		}
+	}
 	cout<<endl;
 }
 int main(){
 	char ch,chara='@';
-	int n=4*size+1,ch1;
-	string alpha ="abcdefghijklmnopqrstuvwxyz";
-//	do{
-//		while(1){
-//			cout<<"1)Change font size\n"
-//				<<"2)Change display character\n"
-//				<<"3)Enter string size\n"
-//				<<"4)Display\n"
-//				<<"Enter choice:";
-//			cin>>ch1;
-//			if(cin.fail()||ch1>4){
-//				cout<<"Error!!\n";
-//				break;
-//			}
-//			if(ch1==1){
-//				cout<<"Enter font size:";
-//				cin>>n;
-//				n=n*2+3;
-//			}
-//			else if(ch1==2){
-//				cout<<"Enter the character:";
-//				cin>>chara;
-//			}
-//			else if(ch1==3){
-//				cout<<"Enter a string:";
-//				cin>>alpha;
-//			}
-//			else
-//			for(int i=0;i<alpha.length();i++){
-//				display(alpha[i],n,chara);
-//				cout<<endl;
-//			}
-//			cout<<endl;
-//		}
-//		
-//		cout<<"Enter 'Y' to continue:";
-//		cin>>ch;
-//	}while(ch=='Y'||ch=='y');
-for(int i=0;i<alpha.length();i++){
-	display(alpha[i],n,chara);
-}
+	int n=n=4*2+1,ch1;
+	string alpha ="welcome";
+	do{
+		while(1){
+			cout<<"1)Change font size\n"
+				<<"2)Change display character\n"
+				<<"3)Enter string\n"
+				<<"4)Display\n"
+				<<"Enter choice:";
+			cin>>ch1;
+			if(cin.fail()||ch1>4){
+				cout<<"Error!!\n";
+				cin.clear();
+                cin.ignore(1,'\n');
+				break;
+			}
+			else if(ch1==1){
+				cout<<"Enter font size:";
+				cin>>n;
+				n=4*n+1;
+			}
+			else if(ch1==2){
+				cout<<"Enter the character:";
+				cin>>chara;
+			}
+			else if(ch1==3){
+				cout<<"Enter a string:";
+				cin.ignore();
+				getline(cin,alpha);
+			}
+			else
+			for(int i=0;i<alpha.length();i++){
+				cout<<endl;
+				display(alpha[i],n,chara);
+			}
+			cout<<endl;
+		}
+		cout<<"Enter 'Y' to continue:";
+		cin>>ch;
+		cout<<endl;
+	}while(ch=='Y'||ch=='y');
 }
